ae74ae69d6353c337239fb050533deab80225eaa
Alisson Costa <alissongabriel.dino@gmail.com>
2018-06-24 14:14:44 -0300

    Alteração do banco de dados. Dentro da tabela de usuários, esqueci de criar uma coluna para data de nascimento. Após este commit está tudo certo

--- DB/dbdomi.sql
+++ DB/dbdomi.sql
@@ -3,15 +3,19 @@ CREATE DATABASE IF NOT EXISTS dbdomi_challenge
 default character set utf8
 default collate utf8_general_ci;
 USE dbdomi_challenge;
+
+
 CREATE TABLE IF NOT EXISTS tb_users(
 	id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name_user VARCHAR(64) NOT NULL,
 	last_name_user VARCHAR(64) NOT NULL,
+	birth_date_user DATE NOT NULL,
 	email_user VARCHAR(64) NOT NULL,
 	login_user VARCHAR(20) NOT NULL,
 	password_user VARCHAR(20) NOT NULL,
 	last_update TIMESTAMP NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS tb_gamers(
 	id_gamer INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	user_id INT NOT NULL,
